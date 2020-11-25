ee45713aa49e1683cd381379ab2438f043fc5bf4
Abhishek S <abhisheksms97@gmail.com>
2020-11-25 19:22:04 +0530

    Create preorder.py

---
+++ preorder.py
@@ -0,0 +1,11 @@
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        res = []
+        self.traverse(root, res)
+        return res
+    
+    def traverse(self, root, res):
+        if root:
+            res.append(root.val)
+            self.traverse(root.left, res)
+            self.traverse(root.right, res)
